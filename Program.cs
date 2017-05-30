f95c6c888b8acf6a5545628151d9007b84c6ed67
XavierTanXY <xaviertanxy@192-168-1-8.tpgi.com.au>
2017-05-30 14:07:08 +0800

    Added C# Program
    Added a program for experimenting async programming

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ProgramAsync {
+	class Program {
+		static void Main() {
+			var demo = new AsyncAwaitDemo();
+			demo.run();
+
+			while (true) {
+				Console.WriteLine("Doing Stuff on the Main Thread...................");
+			}
+		}
+	}
+
+	public class AsyncAwaitDemo {
+		public async Task run() {
+			await Task.Run(() =>
+			{
+				CountOperation();
+			});
+		}
+
+		private static async Task<string> CountOperation() {
+			int counter;
+
+			for (counter = 0; counter < 100000; counter++)
+			{
+				Console.WriteLine(counter);
+			}
+
+			return "Counter = " + counter;
+		}
+	}
+}
